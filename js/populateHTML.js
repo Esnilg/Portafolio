e8596721c53e320254726496da1d35020f11f0a1
Esnil Josue Guevara Mendoza <32929965+Esnilg@users.noreply.github.com>
2022-11-13 18:17:09 -0300

    Update populateHTML.js

--- js/populateHTML.js
+++ js/populateHTML.js
@@ -303,7 +303,7 @@ populateProjects(data.projects.web, "web-projects");
 populateProjects(data.projects.software, "software-projects");
 populateProjects(data.projects.android, "android-projects");
 populateProjects(data.projects.freelance, "freelance-projects");
-populateProjects(data.projects.Energia, "Energia-projects");
+
 
 populateExp_Edu(data.experience, "experience");
 populateExp_Edu(data.education, "education");
