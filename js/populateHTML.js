3e541ff42c07aaf23194f513c2833248a58832ff
Esnil Josue Guevara Mendoza <32929965+Esnilg@users.noreply.github.com>
2022-11-13 17:55:33 -0300

    Update populateHTML.js

--- js/populateHTML.js
+++ js/populateHTML.js
@@ -298,11 +298,12 @@ populateSkills(data.skills, "skills");
 
 fetchBlogsFromMedium();
 
-populateProjects(data.projects.Energía, "Energía-projects");
+
 populateProjects(data.projects.web, "web-projects");
 populateProjects(data.projects.software, "software-projects");
 populateProjects(data.projects.android, "android-projects");
 populateProjects(data.projects.freelance, "freelance-projects");
+populateProjects(data.projects.Energia, "Energia-projects");
 
 populateExp_Edu(data.experience, "experience");
 populateExp_Edu(data.education, "education");
