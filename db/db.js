e36d24ae3da3eb78d7f23e7e77da878ba3630867
Esnil Josue Guevara Mendoza <32929965+Esnilg@users.noreply.github.com>
2022-11-13 17:42:15 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -53,6 +53,26 @@ export default {
 		},
 	],
 	projects: {
+		Energía: [
+			{
+				projectName: "SnylloAir.com",
+				image: "images/snylloair.png",
+				summary:
+					"Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+				preview: "https://www.snylloair.com/",
+				techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+			},
+			{
+				projectName: "Delivery+",
+				image: "images/AM-Logo-.png",
+				summary:
+					"Android Application to display website in android devices.",
+				preview:
+					"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+				techStack: ["Android", "JAVA", "Play Store"],
+			},
+		],
+		
 		web: [
 			{
 				projectName: "Programming Diaries",
